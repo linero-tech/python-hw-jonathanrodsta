bc60b2079dc2f55189d1857e6636556722ffe5d5
jonathanrodsta <jonathan@rodsta.eu>
2022-08-31 10:06:00 +0200

    solution task 3

--- src/lms126/task3.py
+++ src/lms126/task3.py
@@ -2,4 +2,8 @@ from to_do import TODO
 
 
 def task3():
-    TODO("Erase this line and write your solution")
+    TODO()
+    # input for the base of the triangle
+    # input for the height of the triangle
+    # calculate the area by multiplying base * height * 0.5
+    # output the area of the triangle
