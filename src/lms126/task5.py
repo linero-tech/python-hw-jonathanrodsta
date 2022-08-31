7451dfa3fac1ed716437e24c632bb8123a181262
jonathanrodsta <jonathan@rodsta.eu>
2022-08-31 10:52:46 +0200

    solution task 5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -2,4 +2,13 @@ from to_do import TODO
 
 
 def task5():
-    TODO("Erase this line and write your solution")
+    TODO()
+    # input number x 1 to 8
+    # if x = 1 print Monday
+    # if x = 2 print Tuesday
+    # if x = 3 print Wednesday
+    # if x = 4 print Thursday
+    # if x = 5 print Friday
+    # if x = 6 print Saturday
+    # if x = 7 print Sunday
+    # if x = 8 do nothing
