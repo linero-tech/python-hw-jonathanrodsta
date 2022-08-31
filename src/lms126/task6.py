6f203edfd8b557a0b2abd2f701020166a2a0061f
jonathanrodsta <jonathan@rodsta.eu>
2022-08-31 11:19:55 +0200

    solution task 6

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -2,4 +2,12 @@ from to_do import TODO
 
 
 def task6():
-    TODO("Erase this line and write your solution")
+    TODO()
+    # Start counting number x from 1 to 100, inclusive
+    # if x is divisible by 5 print x
+    # Example
+    # x = 3
+    # x / 5 = 0.6 (not divisible by 5 don´t print)
+    # x = 4 (not divisible by 5, don´t print)
+    # x = 5
+    # x / 5 = 1 (Divisible by 5, print x)
