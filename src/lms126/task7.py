82c8f0b43ecaa2ff21646bcc9f207cad6d2d65c7
jonathanrodsta <jonathan@rodsta.eu>
2022-08-31 11:29:27 +0200

    solution task 7

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -2,4 +2,9 @@ from to_do import TODO
 
 
 def task7():
-    TODO("Erase this line and write your solution")
+    TODO()
+    # start counting numbers x from 1 to 100, inclusive
+    # if x is even print x
+    # Example
+    # if x can be divided by 2 print x
+    # if x can´t be divided by 2 dont print x
