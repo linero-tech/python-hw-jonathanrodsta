8a501061bfbab303e6d099afc80a50110d568b9c
jonathanrodsta <jonathan@rodsta.eu>
2022-09-01 11:29:44 +0200

    solution task 9

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,10 @@ from to_do import TODO
 
 
 def task9():
-    TODO("Erase this line and write your solution")
+    TODO()
+    # input user in atm
+    # input the amount you would like to withdraw
+    # if users bank account matching the amount accept
+    # if users bank account does not match the amount deny
+    # if accept ask for a receipt
+    # if accept withdraw the amount
