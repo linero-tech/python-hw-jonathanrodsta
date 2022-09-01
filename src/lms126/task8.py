489df28780e4964042b7d61e6b5f0ae55c61be45
jonathanrodsta <jonathan@rodsta.eu>
2022-09-01 10:46:30 +0200

    solution task 8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,4 +2,9 @@ from to_do import TODO
 
 
 def task8():
-    TODO("Erase this line and write your solution")
+    TODO()
+    # Use modulo operation to see if x is divisible by a certain number
+    # If x modulo 3 equals 0 print Foo
+    # If x modulo 5 equals 0 print bar
+    # If x modulo 3 and 5 equals 0 print FooBar
+    # If x modulo 3 or 5 dones not equal 0 print Qix
