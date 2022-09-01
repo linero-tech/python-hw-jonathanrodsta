0989ce5b5c98ca36d162aad2b55068353d562c06
jonathanrodsta <jonathan@rodsta.eu>
2022-09-01 13:34:03 +0200

    solution task 11

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,11 @@ from to_do import TODO
 
 
 def task11():
-    TODO("Erase this line and write your solution")
+    TODO()
+    # y = ask the person if they want to convert Fahrenheit to Celsius or vice versa
+    # if y=="1"
+    # y = convert Fahrenheit to celsius
+    # output show the temperature in celsius
+    # if y=="2"
+    # y = convert celsius to Fahrenheit
+    # output show temperature in Fahrenheit
