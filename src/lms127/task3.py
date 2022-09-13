f8aac7fe052a5492c995cf6b5a1d51bf76e2151a
jonathanrodsta <jonathan@rodsta.eu>
2022-09-13 10:49:35 +0200

    solution task3

--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -2,6 +2,10 @@ from to_do import TODO
 
 
 def task3(radius):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 2.0 * 3.1416 * radius
+
+    print (result)
+    return result
+
+if __name__ == "__main__":
+    task3(2.0)
