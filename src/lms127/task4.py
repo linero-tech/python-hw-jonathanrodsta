e4834b7746c9cc8074f988ca04793afff1ff87b8
jonathanrodsta <jonathan@rodsta.eu>
2022-09-13 10:58:47 +0200

    solution task4

--- src/lms127/task4.py
+++ src/lms127/task4.py
@@ -2,6 +2,10 @@ from to_do import TODO
 
 
 def task4(base, height):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0.5 * base * height
+    print(result)
+
+    return result
+
+if __name__ == "__main__":
+    task4(5.0,10.0)
