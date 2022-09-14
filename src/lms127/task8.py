3f911c8e51625cfe4035cefb88ce59c5397ec0ab
jonathanrodsta <jonathan@rodsta.eu>
2022-09-14 13:21:34 +0200

    solution task 8

--- src/lms127/task8.py
+++ src/lms127/task8.py
@@ -2,6 +2,9 @@ from to_do import TODO
 
 
 def task8(sentence, character):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = sentence.count(character)
+    print(result)
+    return
+if __name__ == "__main__":
+    task8("I code in KOTLIN","I")
+
