cc96a8b5552ba319bb092efd2d76ff5be0f08737
jonathanrodsta <jonathan@rodsta.eu>
2022-09-28 17:50:25 +0200

    solution task 1

--- src/lms129/task1.py
+++ src/lms129/task1.py
@@ -2,6 +2,22 @@ from to_do import TODO
 
 
 def task1(a, b):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    result = 0
+    boolCheck = False
+    for a in range(a, b + 1):
+        if  a >= b:
+            if (boolCheck):
+                result += a
+            else:
+                result = 0
+                break
+        else:
+            boolCheck = True
+            result += a
+
+    print(result)
+    return (result)
+
+if __name__ == "__main__":
+    task1 (1,5)
