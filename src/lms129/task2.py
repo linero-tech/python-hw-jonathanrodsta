afddb8a12c60d8450bd41155ff9e5842d2f24be2
jonathanrodsta <jonathan@rodsta.eu>
2022-09-28 17:51:22 +0200

    solution task 2

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -2,6 +2,23 @@ from to_do import TODO
 
 
 def task2(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = False
+    if number > 1:
+        # check for factors
+        for i in range(2, number):
+            if (number % i) == 0:
+                # if factor is found, set result to True
+                result = True
+                # break out of loop
+                break
+
+    # check if result is True
+    if result:
+        print(number, "is not a prime number")
+    else:
+        print(number, "is a prime number")
+    return (result)
+
+
+if __name__ == "__main__":
+    task2(5)
