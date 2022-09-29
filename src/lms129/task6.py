120d2882da7b2fa763b5e42a2d1d631288a03d39
jonathanrodsta <jonathan@rodsta.eu>
2022-09-29 14:39:02 +0200

    solution task 6

--- src/lms129/task6.py
+++ src/lms129/task6.py
@@ -2,6 +2,19 @@ from to_do import TODO
 
 
 def task6(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = number
+    rev = 0
+
+
+    while (result > 0):
+        a = result % 10
+        rev = rev * 10 + a
+        result = result // 10
+        print(rev)
+
+    return (result)
+
+
+
+if __name__ == "__main__":
+    task6(678)
