ce5c6cb3cb65652d6ac22741dae2690b951001d8
jonathanrodsta <jonathan@rodsta.eu>
2022-09-29 14:39:21 +0200

    solution task 7

--- src/lms129/task7.py
+++ src/lms129/task7.py
@@ -2,6 +2,12 @@ from to_do import TODO
 
 
 def task7(a, b):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    result = 1
+    for b in range(b, 0, -1):
+        result *= a
+    print(result)
+    return (result)
+
+if __name__ == "__main__":
+    task7(2,3)
