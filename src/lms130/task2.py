424a587767d38b492858fd4bd7aa9963ca364003
jonathanrodsta <jonathan@rodsta.eu>
2022-10-14 19:24:30 +0200

    solution task 2

--- src/lms130/task2.py
+++ src/lms130/task2.py
@@ -2,6 +2,22 @@ from to_do import TODO
 
 
 def task2(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    result = 0
+    for index, number in enumerate(items):
+        if index % 2 == 0:
+            result += number
+    print(result)
+
+    return result
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    task2([1,2,3,4])
