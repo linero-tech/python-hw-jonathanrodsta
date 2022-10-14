75d8ca1c1d0bab1101d785ad75e5dbf8e3e5cbfd
jonathanrodsta <jonathan@rodsta.eu>
2022-10-14 19:21:19 +0200

    solution task 8

--- src/lms130/task8.py
+++ src/lms130/task8.py
@@ -2,6 +2,18 @@ from to_do import TODO
 
 
 def task8(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    num = sorted([(x, i) for (i, x) in enumerate(items)], reverse=True)
+    result = []
+    for x, i in num:
+        if i & x not in result:
+            result.append(x)
+            if len(result) == 3:
+                break
+
+    print(result)
+    return result
+
+
+
+if __name__ == "__main__":
+    task8([60,9,7,10,1222,111,33])
