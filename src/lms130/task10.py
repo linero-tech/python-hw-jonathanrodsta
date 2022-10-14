8da41efd52eff6437fd40e02ff14bff45e9a898b
jonathanrodsta <jonathan@rodsta.eu>
2022-10-14 19:23:23 +0200

    solution task 10

--- src/lms130/task10.py
+++ src/lms130/task10.py
@@ -2,6 +2,20 @@ from to_do import TODO
 
 
 def task10(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    even = 2
+    odd = 3
+    result = []
+
+    for index in range(len(items)):
+
+        if (items[index] % 2 == 0):
+            result.append(items[index]*even)
+
+        else:
+            result.append(items[index]*odd)
+
+    print(result)
+    return result
+if __name__ == "__main__":
+    task10([1,2,3,4,5])
