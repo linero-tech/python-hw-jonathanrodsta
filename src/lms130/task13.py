e6595b2cf4756db4c28ce0c04765f689c6de5610
jonathanrodsta <jonathan@rodsta.eu>
2022-10-14 19:24:07 +0200

    solution task 13

--- src/lms130/task13.py
+++ src/lms130/task13.py
@@ -2,21 +2,55 @@ from to_do import TODO
 
 
 def task13_1(customers):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = list(dict.fromkeys(customers))
+
+    print(result)
+    return result
+
+
+
+if __name__ == "__main__":
+    task13_1(["mary@tv.com","john@nas.gov","john@nas.gov","ema@post.com","ema@post.com"])
 
 
 def task13_2(customers):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+
+    x = customers
+    y = set(x)
+
+    for number in y:
+        occurences = x.count(number)
+        if (occurences > 1):
+            result.append(number)
+
+    print(result)
+    return result
+
+
+
+if __name__ == "__main__":
+    task13_2(["mary@tv.com","john@nas.gov","john@nas.gov","ema@post.com","ema@post.com"])
 
 
 def task13_3(customers):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+
+    start = "@"
+    end = "."
+
+    for email in range(len(customers)):
+        text = customers[email]
+        result.append(text[text.index(start) + len(start):text.index(end)])
+
+    result = list(dict.fromkeys(result))
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task13_3(["mary@tv.com","john@nas.gov","john@nas.gov","ema@post.com","ema@post.com"])
+
 
 
 if __name__ == "__main__":
