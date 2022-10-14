6ff1ea56163b96e20d4f6a6cbd7be0a05170774c
jonathanrodsta <jonathan@rodsta.eu>
2022-10-14 19:17:35 +0200

    solution task 1

--- src/lms130/task1.py
+++ src/lms130/task1.py
@@ -1,7 +1,22 @@
+import random
+
 from to_do import TODO
 
 
 def task1(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    is_list_empty = len(items) == 0
+
+    if is_list_empty:
+        result = 0
+    else:
+        start_index = 0
+        last_index = len(items) - 1
+
+        random_index = random.randint(start_index, last_index)
+
+        result = items[random_index]
+
+    return result
+
+if __name__ == "__main__":
+    print(task1([6,12,60,100]))
