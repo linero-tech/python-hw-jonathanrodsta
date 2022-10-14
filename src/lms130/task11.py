7596967b5c1edbc47022b0d47657b8ab3a07d825
jonathanrodsta <jonathan@rodsta.eu>
2022-10-14 19:23:36 +0200

    solution task 11

--- src/lms130/task11.py
+++ src/lms130/task11.py
@@ -2,21 +2,31 @@ from to_do import TODO
 
 
 def task11_1(guests):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    result = len(guests)
+
+    print(result)
+    return result
+if __name__ == "__main__":
+    task11_1(["Lia-K", "Mar-A", "Luz-K","Ulf-V",])
 
 
 def task11_2(guests, condition):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+    for num in range(len(guests)):
+        if (condition in guests[num]):
+            result.append(guests[num])
+
+    print(result)
+    return result
+
 
 
 if __name__ == "__main__":
+    task11_2( ["Lia-K", "Mar-A", "Luz-K", "Ulf-V"],"-K")
 
     # Change the situation to either "-V", "-A", or "-K" to test your code under different situation
-    situation = "-V"
+    situation = "-v"
     guest_list = [
         "Stéphanie-A",
         "Edmée-K",
