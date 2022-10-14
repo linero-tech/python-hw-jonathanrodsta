94c7c7262a3fff501537059e7ad4ac67deafa663
jonathanrodsta <jonathan@rodsta.eu>
2022-10-14 19:19:31 +0200

    solution task 4

--- src/lms130/task4.py
+++ src/lms130/task4.py
@@ -2,6 +2,17 @@ from to_do import TODO
 
 
 def task4(items, factor):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+
+    for i in items:
+        if (i % factor == 0):
+            result.append(i)
+
+    print(result)
+    return result
+
+
+
+
+if __name__ == "__main__":
+    task4([1,2,3,4,5,6],2)
