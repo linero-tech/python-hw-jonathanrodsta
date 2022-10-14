6c7e448882054251f377df002f6a55f27182089c
jonathanrodsta <jonathan@rodsta.eu>
2022-10-14 19:18:53 +0200

    solution task 3

--- src/lms130/task3.py
+++ src/lms130/task3.py
@@ -2,6 +2,19 @@ from to_do import TODO
 
 
 def task3(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    x = items
+    y = set(x)
+    result = []
+    for number in y :
+        occurrences = x.count(number)
+        if (occurrences > 1):
+            result.append(number)
+    print(result)
+    return result
+
+
+
+
+
+if __name__ == "__main__":
+    task3([1,1,1,2,2,3])
