3452f57e22d42cc23aeee4835b9930f57495cdbf
jonathanrodsta <jonathan@rodsta.eu>
2022-10-24 14:23:01 +0200

    task 10

--- src/lms129/task10.py
+++ src/lms129/task10.py
@@ -9,8 +9,8 @@ def task10(password):
     pattern3 = "[0-9]"
     pattern4 = "[$#@]"
 
-    has_lowercase = re.findall(pattern1,password)
-    has_uppercase = re.findall(pattern2,password)
+    has_lowercase = re.findall(pattern1, password)
+    has_uppercase = re.findall(pattern2, password)
     has_number = re.findall(pattern3, password)
     has_special_character = re.findall(pattern4, password)
     has_correct_length = len(password) in range(6,11)
@@ -18,7 +18,6 @@ def task10(password):
     result = has_lowercase and has_uppercase and has_number and has_special_character and has_correct_length
     return result
 
-if __name__ == "__main__":
-    print(task10("Hejsan@1"))
+
 
 
