ee38a4913ae66a2ce4d175bdbd9ddbf5f3d0d426
jonathanrodsta <jonathan@rodsta.eu>
2022-10-24 18:54:02 +0200

    task 7

--- src/lms130/task7.py
+++ src/lms130/task7.py
@@ -2,15 +2,21 @@ from to_do import TODO
 
 
 def task7(items):
-    result = items[0]
-    for index in range(len(items)):
-        if (items[index] > result):
-            result = result
-        else:
-            result = items[index]
-    print(result)
-    return result
+    result = 0
+    if (len(items) != 0):
+
+        result = items[0]
+        for index in range(len(items)):
+
+            if (items[index] > result):
+                result = result
+            else:
+                result = items[index]
+        print(result)
+    else:
+        result = 0
+        print(result)
 
 
 if __name__ == "__main__":
-    task7([10,5,11])
+    task7([])
