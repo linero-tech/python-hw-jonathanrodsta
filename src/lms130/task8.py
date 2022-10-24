f77e338eef086c94431368049e99836e15708ddc
jonathanrodsta <jonathan@rodsta.eu>
2022-10-24 15:05:24 +0200

    task 8

--- src/lms130/task8.py
+++ src/lms130/task8.py
@@ -2,18 +2,21 @@ from to_do import TODO
 
 
 def task8(items):
-    num = sorted([(x, i) for (i, x) in enumerate(items)], reverse=True)
-    result = []
-    for x, i in num:
-        if i & x not in result:
-            result.append(x)
-            if len(result) == 3:
-                break
+    #num = sorted([(x, i) for (i, x) in enumerate(items)], reverse=True)
+    #result = []
+    #for x, i in num:
+        #if i & x not in result:
+            #result.append(x)
+            #if len(result) == 3:
+                #break
+    items_as_set = set(items)
+    it = list(items_as_set)
+    it.sort()
 
-    print(result)
-    return result
+
+    return it[-3:]
 
 
 
 if __name__ == "__main__":
-    task8([60,9,7,10,1222,111,33])
+    print(task8([60,9,7,10]))
