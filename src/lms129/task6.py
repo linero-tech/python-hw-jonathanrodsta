9a2de86e4c318858c0eee4ad38c0ac66bfada35a
jonathanrodsta <jonathan@rodsta.eu>
2022-10-24 19:27:18 +0200

    task6

--- src/lms129/task6.py
+++ src/lms129/task6.py
@@ -2,19 +2,25 @@ from to_do import TODO
 
 
 def task6(number):
-    result = number
-    rev = 0
+    result = 0
+    if (number % 10 == 0):
+        print(number, " is divisble by 10 sorry! ")
+    else:
+        tempStr = str(number)
 
+        temp = tempStr[::-1]
 
-    while (result > 0):
-        a = result % 10
-        rev = rev * 10 + a
-        result = result // 10
-    print(rev)
+        tempNum = int(temp)
+        tempInt = int(temp)
+        while tempInt > 0:
+            if (tempInt == tempNum):
+                result = tempInt
+            tempInt = tempInt - 1
+    print(result)
 
     return result
 
 
 
 if __name__ == "__main__":
-    task6(284)
+    task6(678)
