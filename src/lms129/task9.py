3a666602102026099cd899e18cca341fb25ed9b2
jonathanrodsta <jonathan@rodsta.eu>
2022-10-24 14:14:08 +0200

    task 9

--- src/lms129/task9.py
+++ src/lms129/task9.py
@@ -2,14 +2,17 @@ from to_do import TODO
 
 
 def task9(temperature):
+
     result = 0
+
     if temperature[-1] == "C" or temperature[-1] == "c":
-        result= (int(temperature[0:-1]) * 9/5) + 32
+        result = (int(temperature[0:-1]) * 9/5) + 32
         result = str(int(result)) + "F"
+
     elif temperature[-1] == "F" or temperature[-1] == "f":
         result = (int(temperature[0:-1]) - 32) * 5/9
         result = str(int(result)) + "C"
-        print(result)
-        return result
+
+    return result
 if __name__ == "__main__":
-    task9("50F")
+   print(task9("-30C"))
