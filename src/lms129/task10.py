fd148f07734d3567534d8818d09daaf5645aff73
jonathanrodsta <jonathan@rodsta.eu>
2022-10-25 18:07:16 +0200

    task 10

--- src/lms129/task10.py
+++ src/lms129/task10.py
@@ -15,9 +15,16 @@ def task10(password):
     has_special_character = re.findall(pattern4, password)
     has_correct_length = len(password) in range(6,11)
 
-    result = has_lowercase and has_uppercase and has_number and has_special_character and has_correct_length
+
+    result =  has_lowercase and has_uppercase and has_number and has_special_character and has_correct_length
+    if (result != True):
+        result = False
     return result
 
 
+
+
+if __name__ == "__main__":
+    print(task10("Hejsan@1"))
 
 
