db1d561836312188fc9e4569ace3083ce1b623bb
jonathanrodsta <jonathan@rodsta.eu>
2022-11-09 12:56:56 +0100

    task 14

--- src/lms131/task14.py
+++ src/lms131/task14.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task14(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    sentence = sentence.lower()
+
+    result = max(set(sentence), key=sentence.count)
+
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task14("I am it")
