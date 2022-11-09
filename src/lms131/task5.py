7adc17d13001d8b7fb45bea972ffc80e6ab96760
jonathanrodsta <jonathan@rodsta.eu>
2022-11-09 12:55:35 +0100

    task 5

--- src/lms131/task5.py
+++ src/lms131/task5.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task5(mapa, constant):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+    for key, mapa_value in mapa.items():
+        if constant != mapa_value:
+            result.append(key)
+    print(result)
+    return result
+
+if __name__ == "__main__":
+    task5({1: 100, 3: 200, 4: 500}, 100)
+
