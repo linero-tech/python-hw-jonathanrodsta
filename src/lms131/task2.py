44c9e873056093cac2bebac4b68fe0bf1d8d9412
jonathanrodsta <jonathan@rodsta.eu>
2022-11-09 12:55:06 +0100

    task 2

--- src/lms131/task2.py
+++ src/lms131/task2.py
@@ -2,6 +2,15 @@ from to_do import TODO
 
 
 def task2(mapa):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+
+    for key, value in mapa.items():
+        result += key
+
+    return result
+
+
+
+
+if __name__ == "__main__":
+    print(task2({1: 10, 2: 20, 3: 30}))
