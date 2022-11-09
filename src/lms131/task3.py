0d10a1cbc04bda8ac76d70ea27c2afc941f7da87
jonathanrodsta <jonathan@rodsta.eu>
2022-11-09 12:55:15 +0100

    task 3

--- src/lms131/task3.py
+++ src/lms131/task3.py
@@ -2,6 +2,15 @@ from to_do import TODO
 
 
 def task3(a, b):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+    for (ak, av), (bk, bv) in zip(a.items(), b.items()):
+        if ak == bk:
+            result.append(ak)
+
+    print(result)
+
+    return result
+
+
+if __name__ == "__main__":
+    task3({1: "A", 2: "B", 3: "C"}, {1: "A", 2: "B", 4: "D"})
