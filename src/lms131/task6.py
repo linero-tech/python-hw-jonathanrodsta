91d4f87e686c599811f5c4ccb2c47c5d91b18669
jonathanrodsta <jonathan@rodsta.eu>
2022-11-09 12:55:42 +0100

    task 6

--- src/lms131/task6.py
+++ src/lms131/task6.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task6(mapa, parameter):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = {}
+
+    for key, value in mapa.items():
+        if value == parameter:
+            result[key] = parameter
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task6({20: "Ana", 40: "John", 22: "Ana"}, "Ana")
