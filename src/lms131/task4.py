4cf537be3c369c81f9ae2463721f309601b74203
jonathanrodsta <jonathan@rodsta.eu>
2022-11-09 12:55:26 +0100

    task 4

--- src/lms131/task4.py
+++ src/lms131/task4.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task4(mapa, value):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+    for key, mapa_value in mapa.items():
+        if value == mapa_value:
+            result.append(key)
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task4({1: 100, 2: 100, 3: 200}, 100)
