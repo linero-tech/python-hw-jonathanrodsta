06b001c663f9a97ffab81450ae8eed2f57eea599
jonathanrodsta <jonathan@rodsta.eu>
2022-11-09 12:56:31 +0100

    task 11

--- src/lms131/task11.py
+++ src/lms131/task11.py
@@ -2,6 +2,15 @@ from to_do import TODO
 
 
 def task11(mapa, prospect):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = False
+    for key, value in mapa.items():
+        if key == prospect:
+            result = True
+            break
+
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task11({1: "A", 2: "B", 3: "C"}, 1)
