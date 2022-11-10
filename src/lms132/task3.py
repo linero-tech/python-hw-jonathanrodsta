e8e54c6c1b783af74fd034f9d278596b33e95c5a
jonathanrodsta <jonathan@rodsta.eu>
2022-11-10 12:58:50 +0100

    task 3

--- src/lms132/task3.py
+++ src/lms132/task3.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task3(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    result = "*"
+    for character in sentence:
+        if character.lower() in vowels:
+            sentence = sentence.replace(character, result)
+    return sentence
+
+
+if __name__ == "__main__":
+    print(task3("I live in SwEdEn"))
