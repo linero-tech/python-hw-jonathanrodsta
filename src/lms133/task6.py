1d94be58868ead7d00208126d4a6bd8958a55f58
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 16:03:55 +0100

    task6 module 7

--- src/lms133/task6.py
+++ src/lms133/task6.py
@@ -1,3 +1,21 @@
 from to_do import TODO
 
-TODO("Create your function here")
+import string as STR
+
+
+def counter(sting):
+    result = False
+    position_total = 1
+    alphabet_dict = {letter: index for index, letter in enumerate(list(STR.ascii_lowercase))}
+
+    if sting is not None:
+        for character in sting.lower().replace(" ",""):
+            if character in alphabet_dict:
+                position_total += alphabet_dict.get(character)
+    if position_total != 0 and position_total % 2 == 0:
+        result = True
+    return result
+
+
+if __name__ == "__main__":
+    print(counter("alexa"))
