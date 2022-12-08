9953d5018c3e427084c7ddf9fb786070f92af094
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 15:42:23 +0100

    task2 module 7

--- src/lms133/task2.py
+++ src/lms133/task2.py
@@ -1,3 +1,19 @@
 from to_do import TODO
 
-TODO("Create your function here")
+
+def fizzBuzzFoo(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+
+    elif number % 5 == 0:
+
+        return "Buzz"
+
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return "Foo"
+
+
+if __name__ == "__main__":
+    print(fizzBuzzFoo(5))
