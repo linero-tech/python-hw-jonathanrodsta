faabb394d1587df152c4dcc036602294c3b012c2
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 15:37:09 +0100

    task 6 module 6

--- src/lms132/task6.py
+++ src/lms132/task6.py
@@ -2,6 +2,20 @@ from to_do import TODO
 
 
 def task6(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    result = []
+    if (items == ""):
+        result = []
+
+    for index, item in enumerate(items):
+        if (items[index] == None):
+            result.append(None)
+        else:
+            result.append(index * items[index])
+
+
+    print(result)
+    return result
+
+if __name__ == "__main__":
+    task6([0,2,None])
