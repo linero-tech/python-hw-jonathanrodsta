f04dc54e736e9f752cc8a0783639921b0d0d015e
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 13:10:26 +0100

    task 1

--- src/lms135/task1.py
+++ src/lms135/task1.py
@@ -1,3 +1,17 @@
 from to_do import TODO
 
-TODO("Create your class here")
+
+class Rectangle:
+    def __init__(self, base, height):
+        self.__base = base
+        self.__height = height
+
+    def perimeter(self):
+        return 2.0 * (self.__base + self.__height)
+
+    def area(self):
+        return self.__base * self.__height
+if __name__ == "__main__":
+    rect1 = Rectangle(base=1, height=1)
+    print(rect1.perimeter())
+    print(rect1.area())
