90c8d3fbabceca8c49732c979a797b73460bff6e
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 15:20:51 +0100

    task2 module 106

--- src/lms132/task2.py
+++ src/lms132/task2.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task2(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = items.count(None)
+    if result == "":
+        result = 0
+        print(result)
+
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task2([1, None, 2, None, 3])
