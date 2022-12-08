d739ae929071398593db231d4d4090c501847800
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 15:36:32 +0100

    task 5 module 6

--- src/lms132/task5.py
+++ src/lms132/task5.py
@@ -2,6 +2,19 @@ from to_do import TODO
 
 
 def task5(word):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    if word == "":
+        return False
+    if word == None:
+        return False
+    clean_word = word.lower()
+    result = []
+    for letter in clean_word:
+        if letter.isalpha():
+            if letter in result:
+                return False
+            result.append(letter)
+    return True
+
+if __name__ == "__main__":
+    print(task5("space"))
+
