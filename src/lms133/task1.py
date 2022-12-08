109f5eb65f4611b5f49e70c3beb314357011a937
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 15:41:42 +0100

    task1 module 7

--- src/lms133/task1.py
+++ src/lms133/task1.py
@@ -1,3 +1,16 @@
 from to_do import TODO
 
-TODO("Create your function here")
+
+def remover(sentence):
+    result = ""
+    vowels = ["a", "e", "i", "o", "u"]
+
+    for letter in sentence:
+        if letter.lower() not in vowels:
+            result += letter
+
+
+
+    return result
+if __name__ == "__main__":
+    print(remover("Sweden is nice"))
