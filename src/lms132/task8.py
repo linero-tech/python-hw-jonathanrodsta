c761821e742ec2545cf4a64fab4c3c86cd5bb503
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 15:38:35 +0100

    task 8 module 6

--- src/lms132/task8.py
+++ src/lms132/task8.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task8(items):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = []
+
+    for i in items:
+        if i is not None and len(i) > 3:
+            result.append(i)
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task8(["Hello", "Gothenburg",None])
