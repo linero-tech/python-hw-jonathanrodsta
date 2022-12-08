7fa4988e9d7e976644c930283b6a1a54c357856e
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 15:37:45 +0100

    task 7 module 6

--- src/lms132/task7.py
+++ src/lms132/task7.py
@@ -2,6 +2,17 @@ from to_do import TODO
 
 
 def task7(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = {}
+    t = 0
+    for i in sentence:
+        if (i.isalpha()):
+            t += 1
+    result["letters"] = t
+    result["digits"] = len(sentence) - t
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task7("H3ll0")
+
