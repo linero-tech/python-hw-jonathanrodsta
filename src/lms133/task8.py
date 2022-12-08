33f514d9a7c57b18f53136ae9e99e8c093eda394
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 16:09:11 +0100

    task 8 module 7

--- src/lms133/task8.py
+++ src/lms133/task8.py
@@ -1,3 +1,23 @@
 from to_do import TODO
 
-TODO("Create your function here")
+
+def vowels(string):
+    count = 0
+    string = string.lower()
+    for letter in string:
+        if (letter == "a"):
+            count += 5
+        elif(letter == "e"):
+            count += 4
+        elif(letter == "i"):
+            count += 3
+        elif(letter == "o"):
+            count += 2
+        elif(letter == "u"):
+            count += 1
+    return count
+
+
+
+if __name__ == "__main__":
+    print(vowels("I love kotlin"))
