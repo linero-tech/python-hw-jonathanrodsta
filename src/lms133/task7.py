d34766079b3b48492dd158ece3902adc36bc6c1d
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 16:08:27 +0100

    task7 module 7

--- src/lms133/task7.py
+++ src/lms133/task7.py
@@ -1,3 +1,18 @@
 from to_do import TODO
 
-TODO("Create your function here")
+def censorship(text):
+    result = []
+    for word in text.split(" "):
+        if len(word) > 4:
+            result.append("*" * len(word))
+        else:
+            result.append(word)
+    return " ".join(result)
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(censorship("I live in Sweden"))
