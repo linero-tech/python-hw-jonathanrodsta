3460c84ed33a7c545a0fc6ee2291db1bdb9c38f1
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 16:02:24 +0100

    task5 module7

--- src/lms133/task5.py
+++ src/lms133/task5.py
@@ -1,3 +1,28 @@
 from to_do import TODO
 
-TODO("Create your function here")
+# Define the function signature
+from typing import List, Dict ,Any
+
+def groceries(grocery_list: List[Dict[str, Any]]) -> float:
+  total_price = 0.0
+  for grocery in grocery_list:
+    # Get the quantity and price from the map
+    quantity = grocery["quantity"]
+    price = grocery["price"]
+
+    # Calculate the total price for this item
+    item_total = quantity * price
+
+    # Add the total price to the shopping cart total
+    total_price += item_total
+  return total_price
+
+
+
+
+
+if __name__ == "__main__":
+    shopping_cart = [{"product": "Milk","quantity": 1, "price": 1.50}]
+    total_price = groceries(shopping_cart)
+    print("Total price:", total_price)
+
