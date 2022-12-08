95b22cd004675b7e57047329f241b71c7abbcb3d
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 13:10:34 +0100

    task 2

--- src/lms135/task2.py
+++ src/lms135/task2.py
@@ -1,3 +1,23 @@
 from to_do import TODO
 
-TODO("Create your class here")
+class Account:
+    def __init__(self, balance: float):
+        self.balance = balance
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    def deposit(self, amount: float):
+        self.__balance += amount
+
+    def withdrawal(self, amount: float):
+        if self.__balance >= amount:
+            self.__balance -= amount
+
+    def fee(self):
+        self.__balance *= 0.95
+
+
+if __name__ == "__main__":
+
