6e313b1b126c2c0e152b0b53a29f4fbd689bd75b
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 16:11:13 +0100

    task 9 module 7

--- src/lms133/task9.py
+++ src/lms133/task9.py
@@ -1,3 +1,25 @@
 from to_do import TODO
 
-TODO("Create your function here")
+def harshad(number):
+    if number == 0:
+        return False
+
+    num = number
+    sum_of_digits = 0
+
+    while num != 0:
+        last_digit = num % 10
+
+        sum_of_digits += last_digit
+
+        num = int(num / 10)
+
+    return number % sum_of_digits == 0
+
+
+
+
+if __name__ == "__main__":
+    print(harshad(1))
+
+
