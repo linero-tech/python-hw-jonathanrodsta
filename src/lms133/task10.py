70993de0201a7143e3d94617cc95f1a6e822b79a
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 16:15:22 +0100

    task10 module 7

--- src/lms133/task10.py
+++ src/lms133/task10.py
@@ -1,3 +1,17 @@
 from to_do import TODO
 
-TODO("Create your function here")
+
+def tweet(message: str) -> str:
+    # get   the length of text
+    # number of @ symbol needed = 280 - length
+    # multiple the "@" symbol * the amount we need result.append("*" * len(word))
+    # append to the end of the sentence the result from previous step
+    remaining_chars = 280 - len(message)
+
+    filler = "@" * remaining_chars
+
+    return message + filler
+
+
+if __name__ == "__main__":
+    print(tweet("I feel good today!"))
