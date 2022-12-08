068f086e331cf95dd2d9a8f6e317b2a8f754f51b
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 14:42:43 +0100

    task8 module 105

--- src/lms131/task8.py
+++ src/lms131/task8.py
@@ -2,6 +2,36 @@ from to_do import TODO
 
 
 def task8(grades):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    grades = [
+        {"Ana": [4.0, 4.5, 5.0]},
+        {"John": [5.0, 5.0, 3.0]},
+        {"Lise": [5.0, 5.0, 5.0]},
+    ]
+
+    highest_average = 0
+    result = ""
+
+    for student_grades in grades:
+        for student, grades in student_grades.items():
+
+            total = 0
+            for grade in grades:
+                total += grade
+            average = total / len(grades)
+
+
+            if average > highest_average:
+                highest_average = average
+                result = student
+
+    return result
+
+if __name__ == "__main__":
+    print(task8(" "))
+
+
+
+
+
+
+
