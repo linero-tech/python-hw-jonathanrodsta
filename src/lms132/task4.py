dc2bb9438b4590ef0a2317a02480fff11191c496
jonathanrodsta <jonathan@rodsta.eu>
2022-12-08 15:33:40 +0100

    task 4 module 6

--- src/lms132/task4.py
+++ src/lms132/task4.py
@@ -2,6 +2,17 @@ from to_do import TODO
 
 
 def task4(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = dict()
+
+    if sentence is not None and sentence != "":
+        result = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
+
+        for character in sentence.lower():
+            if character in result:
+                result[character] += 1
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    task4("Hello")
