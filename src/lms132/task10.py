11afbddd5287c7e7ea117df8d8845c71326b31c4
jonathanrodsta <jonathan@rodsta.eu>
2022-12-12 15:35:48 +0100

    module6 task 10

--- src/lms132/task10.py
+++ src/lms132/task10.py
@@ -2,16 +2,16 @@ from to_do import TODO
 
 
 def task10(items):
+
+
     result = None
+    for i in range(len(items)):
+        if items[i] == "Nemo":
+            result = i
+            break
 
-    for index, element in enumerate(items):
-        if element.lower() == "nemo":
-            result = index
-    print(result)
+    print(result)  # should print 2
     return result
-
-
-
 
 
 if __name__ == "__main__":
