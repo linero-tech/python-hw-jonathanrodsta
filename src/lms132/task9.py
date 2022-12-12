b3d223829cb1fff4ab15bc8207a17e66917cb52a
jonathanrodsta <jonathan@rodsta.eu>
2022-12-12 15:08:59 +0100

    module6 task 9

--- src/lms132/task9.py
+++ src/lms132/task9.py
@@ -9,14 +9,12 @@ def task9(identification):
         month = int(identification[4:6])
         day = int(identification[6:8])
 
-        if year > 1947 and month in range (1, 13) and day in range (1, 32) and len(identification) == 13:
+        if year >= 1947 and month in range(1, 13) and day in range(1, 32) and len(identification) == 13:
             result = True
     return result
-
-
-
 
 
 if __name__ == "__main__":
     print(task9("19921117-9078"))
     print(task9(None))
+    print(task9("19470110-1739"))
