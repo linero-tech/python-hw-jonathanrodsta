df5f24b69f7a8fb63505c95ec1d518c5b500343e
jonathanrodsta <jonathan@rodsta.eu>
2022-12-12 14:17:05 +0100

    module 6 task 2

--- src/lms132/task2.py
+++ src/lms132/task2.py
@@ -2,37 +2,12 @@ from to_do import TODO
 
 
 def task2(items):
-    # Initialize the counter variable to 0
     result = 0
-
-    # Iterate over the items list
     for item in items:
-        # Check if the current element is null
-        if item == None:
-            # Increment the counter variable
+
+        if item is None:
             result += 1
-
-    # Print the result
     print(result)
-
-    # Check if the items list is null
-    if items == None:
-        # Assign a value of 0 to the result variable
-        result = 0
-    else:
-        # Initialize the counter variable to 0
-        result = 0
-
-        # Iterate over the items list
-        for item in items:
-            # Check if the current element is null
-            if item == None:
-                # Increment the counter variable
-                result += 1
-
-    # Print the result
-    print(result)
-
     return result
 
 
