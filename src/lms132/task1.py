376e99eb4541f78fd45c6526fcdce542fc85ee73
jonathanrodsta <jonathan@rodsta.eu>
2022-12-12 14:14:34 +0100

    module 6 task 1

--- src/lms132/task1.py
+++ src/lms132/task1.py
@@ -4,21 +4,12 @@ from to_do import TODO
 def task1(sentence):
     result = len(sentence)
 
-    print(result)
-
-    sentence = "I love GBG"
-
-    # Check if sentence is None
     if sentence is None:
-        # If it is, then assign a value of 0 to result
         result = 0
     else:
-        # Otherwise, calculate the length of the string
         result = len(sentence)
 
-    # Print the result
     print(result)
-
     return result
 
 
