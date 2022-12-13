1cb42dd7ee8f892c3db568ec5cd8f9645024a104
jonathanrodsta <jonathan@rodsta.eu>
2022-12-13 14:34:20 +0100

    module 5 task 13

--- src/lms131/task13.py
+++ src/lms131/task13.py
@@ -2,18 +2,33 @@ from to_do import TODO
 
 
 def task13(sentence):
-    sentence = sentence.lower()
-    words = sentence.split(" ")
-    dict = {}
+    # Split the sentence into words and remove special characters
+    words = sentence.split()
+    for i, word in enumerate(words):
+        words[i] = word.strip(".,:;").lower()
 
+    # Count the frequency of each word
+    word_counts = {}
     for word in words:
-        word = word.replace(",", "").replace(";", "").replace(".", "").replace(":", "")
-        word = word.lower()
-        dict[word] = words.count(word)
-    result = max(dict, key=dict.get)
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+    # Find the word with the highest frequency
+    max_count = 0
+    max_word = ""
+    for word, count in word_counts.items():
+        if count > max_count:
+            max_count = count
+            max_word = word
+
+    # Save the result in lowercase
+    result = max_word.lower()
+
     print(result)
     return result
 
 
 if __name__ == "__main__":
-    task13("this This THIS is still the very very same")
+    task13('this, This; THIS, is still the very very same')
