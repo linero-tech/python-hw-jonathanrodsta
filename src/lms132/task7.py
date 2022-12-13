c2e4cc794cde819864d0a92c0f60ed3fc5f0cc70
jonathanrodsta <jonathan@rodsta.eu>
2022-12-13 15:45:26 +0100

    module 6 task7

--- src/lms132/task7.py
+++ src/lms132/task7.py
@@ -2,18 +2,31 @@ from to_do import TODO
 
 
 def task7(sentence):
-    result = {"letters": 0, "digits": 0}
-    for char in sentence:
-        if char.isalpha():
-            result["letters"] += 1
-        elif char.isdigit():
-            result["digits"] += 1
+    # Initialize the counts
+    letters = 0
+    digits = 0
 
+    # Check if sentence is not null
+    if sentence is not None:
+        # Iterate over each character in sentence
+        for char in sentence:
+            # Check if the character is a letter
+            if char.isalpha():
+                letters += 1
+            # Check if the character is a digit
+            elif char.isdigit():
+                digits += 1
+
+    # Save the counts in result
+    result = {
+        "letters": letters,
+        "digits": digits
+    }
     print(result)
 
     return result
 
 
 if __name__ == "__main__":
-    task7("H3ll0")
+    task7(None)
 
