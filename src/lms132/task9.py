78469c72697c480143328d7720f3c0475ec33ba5
jonathanrodsta <jonathan@rodsta.eu>
2022-12-13 15:41:17 +0100

    module6 task9

--- src/lms132/task9.py
+++ src/lms132/task9.py
@@ -4,19 +4,25 @@ from to_do import TODO
 def task9(identification):
     result = False
 
-    if identification is not None:
-        year = int(identification[0:4])
-        month = int(identification[4:6])
-        day = int(identification[6:8])
+    if identification is None:
+        result = False
+    else:
+        try:
+            year = int(identification[0:4])
+            month = int(identification[4:6])
+            day = int(identification[6:8])
 
-        if year > 1946 and month in range(1, 13) and day in range(1, 32) and len(identification) == 13:
-            result = True
+            if year >= 1947 and (1 <= month <= 12) and (1 <= day <= 31) and len(identification) == 13:
+                result = True
+        except ValueError as ve:
+            result = False
     return result
 
 
 if __name__ == "__main__":
-    print(task9("19921117-9078"))
-    print(task9(None))
-    print(task9("19470110-1739"))
-    print(task9("19923131-1090"))
-    print(task9('19460511-1084'))
+    print(task9("19922331-1090"))  # wrong month False
+    print(task9(None))  # False
+    print(task9("19200112-1090"))  # wrong year False
+    print(task9("1992-3131-1090"))  # False wrong PN
+    print(task9('19920189-1090'))  # wrong day False
+    print(task9("19921117-9078"))  # True
