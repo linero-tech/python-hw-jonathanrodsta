50f415c04cc84dd50ed93b62a89f975ccc1cfc59
jonathanrodsta <jonathan@rodsta.eu>
2022-12-13 15:50:16 +0100

    module6 task3

--- src/lms132/task3.py
+++ src/lms132/task3.py
@@ -2,18 +2,24 @@ from to_do import TODO
 
 
 def task3(sentence):
-    vowels = ["a", "e", "i", "o", "u"]
+    # Initialize an empty string to store the modified sentence
+    modified_sentence = ""
 
-    if sentence == None:
-        result = "*"
-    else:
-        result = sentence
+    # Check if sentence is not null
+    if sentence is not None:
+        # Iterate over each character in sentence
+        for char in sentence:
+            # Check if the character is a vowel
+            if char.lower() in "aeiou":
+                # Replace the character with an asterisk (*)
+                modified_sentence += "*"
+            else:
+                # Otherwise, keep the character as is
+                modified_sentence += char
 
-        for letter in sentence:
-            if letter.lower() in vowels:
-                result = result.replace(letter, "*")
-    return result
+    # Return the modified sentence
+    return modified_sentence
 
 
 if __name__ == "__main__":
-    print(task3(None))
+    print(task3("I live in SwEdEn"))
