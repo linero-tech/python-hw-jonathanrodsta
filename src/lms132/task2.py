b1c1a81c1206c2d0c4899f893fde2b37ba9cf96e
jonathanrodsta <jonathan@rodsta.eu>
2022-12-13 16:07:44 +0100

    module6 task2

--- src/lms132/task2.py
+++ src/lms132/task2.py
@@ -4,14 +4,9 @@ from to_do import TODO
 def task2(items):
     result = 0
     for item in items:
-
         if item is None:
             result += 1
-
-
-
     return result
 
-
 if __name__ == "__main__":
-    print(task2([1, None, 2, None, 3]))
+    print(task2([1,None,  2,None, 3]))
