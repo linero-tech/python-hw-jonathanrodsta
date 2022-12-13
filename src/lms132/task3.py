2e57b499eab2c3405ebe59014e303264b3464ac2
jonathanrodsta <jonathan@rodsta.eu>
2022-12-13 15:12:10 +0100

    module6 task3

--- src/lms132/task3.py
+++ src/lms132/task3.py
@@ -2,7 +2,7 @@ from to_do import TODO
 
 
 def task3(sentence):
-    vowels = ['a', 'e', 'i', 'o', 'u']
+    vowels = ["a", "e", "i", "o", "u"]
 
     if sentence == None:
         result = "*"
@@ -16,4 +16,4 @@ def task3(sentence):
 
 
 if __name__ == "__main__":
-    print(task3("I live in SwEdEN"))
+    print(task3(None))
